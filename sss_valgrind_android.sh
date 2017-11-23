5b1188a54de87b6d235baae22fc896220ec236f6
sunShuShu <13247774+sunShuShu@users.noreply.github.com>
2017-11-23 15:17:15 +0800

    Handle input guide; Finish all base function;

--- sss_valgrind_android.sh
+++ sss_valgrind_android.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 install_valgrind() {
-	adb root
+	DEVICE_CPU_INFO=`adb shell cat /proc/cpuinfo`
 	if [ $? -ne 0 ]; then
 		echo "FAIL! Ensure adb is installed and device is pluged!"
 		exit 1
 	fi
-	DEVICE_CPU_INFO=`adb shell cat /proc/cpuinfo`
 	DEVICE_CPU_INDEX=`echo $DEVICE_CPU_INFO | awk -F"AArch64" '{print $1}' | wc -c`
 	if [ $DEVICE_CPU_INDEX -ge ${#DEVICE_CPU_INFO} ]; then
 		DEVICE_CPU_INDEX=`echo $DEVICE_CPU_INFO | awk -F"ARMv7" '{print $1}' | wc -c`
@@ -23,7 +22,7 @@ install_valgrind() {
 	echo "DEVICE_CPU_TYPE: $DEVICE_CPU_TYPE"
 	
 	rm -r ./Inst
-	echo "unzip valgrind package..."
+	echo "Unzip valgrind package..."
 	case $DEVICE_CPU_TYPE in
 		"ARMv7")
 			unzip -nq valgrind-arm.zip
@@ -41,23 +40,83 @@ install_valgrind() {
 	adb push ./Inst/data/local/Inst /data/local
 	if [[ $? -ne 0 ]]; then
 		echo "FAIL! Can not push valgrind to device."
-		echo "You can try to move the ./Inst/data/local/Inst to /data/local manually."
+		echo "You can try to move the `pwd`/Inst/data/local/Inst to /data/local manually."
 		exit 1
 	else
 		rm -r ./Inst
 	fi
 
 	echo "DONE, install valgrind success!"
-	exit 0
+}
+
+input_parameter() {
+	if [[ ! -e .sss_last_input ]]; then
+	PACKAGE_NAME="com.example.ctest"
+	APP_MAIN_ACTIVITY="CTestActivity"
+	SYMBOLIZED_LIB_PATH="`pwd`/CTest/app/build/intermediates/cmake/debug/obj/armeabi-v7a"
+	echo -e "${PACKAGE_NAME}\n${APP_MAIN_ACTIVITY}\n${SYMBOLIZED_LIB_PATH}" > .sss_last_input
+	fi
+
+	echo "Type valgrind parameter, nullable."
+	echo -e " (You don't need to set \"--log-file\". By default, the log will be written to file(s) and got back automatically.)"
+	read VALGRIND_PARAMS
+	echo "$VALGRIND_PARAMS" | grep -q "log-file"
+	if [[ $? -ne 0 ]]; then
+		VALGRIND_PARAMS="$VALGRIND_PARAMS --log-file=/sdcard/valgrind.%p.log"
+	fi
+
+	PACKAGE_NAME=`sed -n "1p" .sss_last_input`
+	echo "Type the package name of your android app. [$PACKAGE_NAME]"
+	while read PACKAGE_NAME_INPUT; do
+		if [[ -n $PACKAGE_NAME_INPUT ]]; then
+			PACKAGE_NAME="$PACKAGE_NAME_INPUT"
+		else
+			echo "$PACKAGE_NAME"
+		fi
+		ALL_PACKAGES=`adb shell pm list packages`
+		if [[ $? -ne 0 ]]; then
+			echo "Ensure adb is installed and device is pluged!"
+			continue
+		fi
+		echo "$ALL_PACKAGES" | grep -q "package:${PACKAGE_NAME}\r"
+		if [[ $? -ne 0 ]]; then
+			echo "There is no specified app on the device, try again."
+			continue
+		else
+			break
+		fi
+	done
+
+	APP_MAIN_ACTIVITY=`sed -n "2p" .sss_last_input`
+	echo "Type the main avtivity name of the app. [$APP_MAIN_ACTIVITY]"
+	read APP_MAIN_ACTIVITY_INPUT
+	if [[ -n $APP_MAIN_ACTIVITY_INPUT ]]; then
+		APP_MAIN_ACTIVITY="$APP_MAIN_ACTIVITY_INPUT"
+	else
+		echo "$APP_MAIN_ACTIVITY"
+	fi
+
+	SYMBOLIZED_LIB_PATH=`sed -n "3p" .sss_last_input`
+	echo "Type the directory of lib with symbol table. [$SYMBOLIZED_LIB_PATH]"
+	while read SYMBOLIZED_LIB_PATH_INPUT; do
+		if [[ -n $SYMBOLIZED_LIB_PATH_INPUT ]]; then
+			SYMBOLIZED_LIB_PATH="$SYMBOLIZED_LIB_PATH_INPUT"
+		else
+			echo "$SYMBOLIZED_LIB_PATH"
+		fi
+		ALL_SYMBOLIZED_LIB=`ls "$SYMBOLIZED_LIB_PATH" | grep -e ".so" -e ".a"`
+		if [[ -z $ALL_SYMBOLIZED_LIB ]]; then
+			echo "There is not lib file, try again."
+			continue
+		else
+			break
+		fi
+	done
+
+	echo -e "${PACKAGE_NAME}\n${APP_MAIN_ACTIVITY}\n${SYMBOLIZED_LIB_PATH}" > .sss_last_input
 }
 
 push_sh_to_device() {
-	PACKAGE_NAME="com.example.sunda.ctest"
-	VALGRIND_PARAMS="-v --sigill-diagnostics=no --log-file=/sdcard/valgrind.%p.log --leak-check=full --show-reachable=yes"
-	if [[ -z $PACKAGE_NAME ]]; then
-		echo "FAIL! The app package name is not set"
-		exit 1
-	fi
 	START_CALGRIND_SH="#!/system/bin/sh \n
 PACKAGE=\"${PACKAGE_NAME}\" \n
 VGPARAMS='${VALGRIND_PARAMS}' \n
@@ -75,20 +134,15 @@ exec /data/local/Inst/bin/valgrind \$VGPARAMS \$* "
 
 	adb shell chmod 777 /data/local/start_valgrind.sh
 	if [ $? -ne 0 ]; then
-		echo "FAIL! Can not chmod 777 /data/local/start_valgrind.sh"
+		echo "FAIL! Can not execute: adb shell chmod 777 /data/local/start_valgrind.sh"
 		exit 1
 	fi
 }
 
 replace_lib() {
-	SYMBOLIZED_LIB_PATH="/Users/sunda/CTest/app/build/intermediates/cmake/debug/obj/armeabi-v7a/"
-	ALL_SYMBOLIZED_LIB=`ls "$SYMBOLIZED_LIB_PATH"`
-	if [[ -z $ALL_SYMBOLIZED_LIB ]]; then
-		echo "FAIL! There is not symbolized lib."
-		exit 1
-	fi
+	echo "Replace app lib..."
 	echo "$ALL_SYMBOLIZED_LIB" | while read LIB_PATH; do
-		adb push "$SYMBOLIZED_LIB_PATH$LIB_PATH" "/data/data/$PACKAGE_NAME/lib"
+		adb push "$SYMBOLIZED_LIB_PATH/$LIB_PATH" "/data/data/$PACKAGE_NAME/lib"
 		if [[ $? -ne 0 ]]; then
 			echo "FAIL! Can not push symbolized lib to /data/data/$PACKAGE_NAME/lib"
 			exit 1
@@ -110,13 +164,13 @@ launch_valgrind() {
 
 	echo -e "\"$PACKAGE_NAME\" will relaunch..."
 	adb shell am force-stop $PACKAGE_NAME
-	adb shell am start -a android.intent.action.MAIN -n $PACKAGE_NAME/.CTestActivity
+	adb shell am start -a android.intent.action.MAIN -n "$PACKAGE_NAME/.$APP_MAIN_ACTIVITY"
 	clear
 	echo "Launching app..."
 	echo "This may take as long as a song. Have a cup of coffee ☕️ "
 }
 
-wait_terminate_app() {
+wait_and_terminate_app() {
 	while true; do
 		echo -e " (Input \"done\" to terminate app and get back logs.)"
 		read NEXT_ACTION
@@ -141,8 +195,8 @@ pull_logs() {
 	echo "Getting back logs..."
 	ALL_LOGS=`adb shell ls /sdcard/ | grep "valgrind.*.log" | tr -d "\r"`
 	if [[ ${#ALL_LOGS} -lt 13 ]]; then
-		echo "FAIL! There is no log file."
-		exit 1
+		echo "DONE, There is no log file."
+		exit 0
 	fi
 
 	echo "$ALL_LOGS" | while read LOG_PATH; do
@@ -155,18 +209,35 @@ pull_logs() {
 	done
 
 	adb shell rm /sdcard/valgrind.*.log
-	echo "DONE. Please check the log(s) in `pwd`"
+	echo "DONE. Check the log(s) in `pwd`"
 }
 
-#install_valgrind
-#获取所有包名
-#adb shell pm list packages
+
+if [[ $# -eq 0 ]]; then
+	input_parameter
+else
+	if [[ $1 = "-i" ]]; then
+		install_valgrind
+		exit 0
+	fi
+	if [[ $# -eq 4 ]]; then
+		VALGRIND_PARAMS=$1
+		PACKAGE_NAME=$2
+		APP_MAIN_ACTIVITY=$3
+		SYMBOLIZED_LIB_PATH=$4
+	else
+		echo "FAIL! Parameter error."
+		echo "Perhaps you should try to execute this script without any arguments."
+		echo "Or you could use it like this:"
+		echo "sss_valgrind_android.sh \"valgrind_parameters\" \"com.example.test\" \"MainActivity\" \"/lib_with_symbol_table_path\""
+		exit 1
+	fi
+fi
 
 push_sh_to_device
-#replace_lib
+replace_lib
 launch_valgrind
-
-wait_terminate_app
+wait_and_terminate_app
 pull_logs
 
 exit 0
